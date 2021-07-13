0db7ff319348e848c39f0b68a17a6c8221955b06
chanyoungjung <hy910915@kaist.ac.kr>
2021-07-12 23:52:13 -0400

    nif_localization_minimal is updated

--- src/perception/nif_localization_minimal/include/nif_localization_minimal/localization_minimal.h
+++ src/perception/nif_localization_minimal/include/nif_localization_minimal/localization_minimal.h
@@ -9,21 +9,29 @@
 #define NIF_LOCALIZATION_MINIMAL_H
 
 #include "nav_msgs/msg/odometry.hpp"
+#include "nif_common/constants.h"
 #include "nif_frame_id/frame_id.h"
 #include "nif_utils/geodetic_converter.h"
+#include "novatel_gps_msgs/msg/inspva.hpp"
+#include "tf2/LinearMath/Quaternion.h"
 
 class LocalizationMinimal {
 private:
-  /* data */
-
   std::shared_ptr<GeodeticConverter> m_geo_converter_ptr;
+
+  novatel_gps_msgs::msg::Inspva m_gps_horizontal, m_gps_vertical;
+  nav_msgs::msg::Odometry m_veh_odom_horizontal, m_veh_odom_vertical;
+
+  double m_heading_rad_gps_horizontal, m_heading_rad_gps_vertical;
+  double m_heading_deg_gps_horizontal, m_heading_deg_gps_vertical;
 
 public:
   LocalizationMinimal();
   ~LocalizationMinimal();
 
-  void setGPSHorizontalData();
-  void setGPSVerticalData();
+  void
+  setGPSHorizontalData(novatel_gps_msgs::msg::Inspva& gps_horizontal_data_);
+  void setGPSVerticalData(novatel_gps_msgs::msg::Inspva& gps_vertical_data_);
 };
 
 #endif // NIF_LOCALIZATION_MINIMAL_H
--- src/perception/nif_localization_minimal/src/localization_minimal.cpp
+++ src/perception/nif_localization_minimal/src/localization_minimal.cpp
@@ -7,5 +7,59 @@
 
 #include "nif_localization_minimal/localization_minimal.h"
 
-LocalizationMinimal::LocalizationMinimal() {}
+LocalizationMinimal::LocalizationMinimal() {
+  m_geo_converter_ptr = std::make_shared<GeodeticConverter>();
+}
+
 LocalizationMinimal::~LocalizationMinimal() {}
+
+void LocalizationMinimal::setGPSHorizontalData(
+    novatel_gps_msgs::msg::Inspva& gps_horizontal_data_) {
+  m_geo_converter_ptr->geodetic2Ned(
+      gps_horizontal_data_.latitude,
+      gps_horizontal_data_.longitude,
+      0.0,
+      &m_veh_odom_horizontal.pose.pose.position.x,
+      &m_veh_odom_horizontal.pose.pose.position.y,
+      &m_veh_odom_horizontal.pose.pose.position.z);
+
+  // TODO : Not sure about passing the roll,pitch. Maybe we can just pass zero.
+  tf2::Quaternion vehicle_quat;
+  vehicle_quat.setRPY(gps_horizontal_data_.roll,
+                      gps_horizontal_data_.pitch,
+                      gps_horizontal_data_.azimuth);
+  vehicle_quat = vehicle_quat.normalize();
+  m_veh_odom_horizontal.pose.pose.orientation.x = vehicle_quat.x();
+  m_veh_odom_horizontal.pose.pose.orientation.y = vehicle_quat.y();
+  m_veh_odom_horizontal.pose.pose.orientation.z = vehicle_quat.z();
+  m_veh_odom_horizontal.pose.pose.orientation.w = vehicle_quat.w();
+
+  m_heading_deg_gps_horizontal = gps_horizontal_data_.azimuth;
+  m_heading_rad_gps_horizontal =
+      m_heading_deg_gps_horizontal * nif::common::constants::DEG2RAD;
+}
+
+void LocalizationMinimal::setGPSVerticalData(
+    novatel_gps_msgs::msg::Inspva& gps_vertical_data_) {
+  m_geo_converter_ptr->geodetic2Ned(gps_vertical_data_.latitude,
+                                    gps_vertical_data_.longitude,
+                                    0.0,
+                                    &m_veh_odom_vertical.pose.pose.position.x,
+                                    &m_veh_odom_vertical.pose.pose.position.y,
+                                    &m_veh_odom_vertical.pose.pose.position.z);
+
+  // TODO : Not sure about passing the roll,pitch. Maybe we can just pass zero.
+  tf2::Quaternion vehicle_quat;
+  vehicle_quat.setRPY(gps_vertical_data_.roll,
+                      gps_vertical_data_.pitch,
+                      gps_vertical_data_.azimuth);
+  vehicle_quat = vehicle_quat.normalize();
+  m_veh_odom_vertical.pose.pose.orientation.x = vehicle_quat.x();
+  m_veh_odom_vertical.pose.pose.orientation.y = vehicle_quat.y();
+  m_veh_odom_vertical.pose.pose.orientation.z = vehicle_quat.z();
+  m_veh_odom_vertical.pose.pose.orientation.w = vehicle_quat.w();
+
+  m_heading_deg_gps_vertical = gps_vertical_data_.azimuth;
+  m_heading_rad_gps_vertical =
+      m_heading_deg_gps_vertical * nif::common::constants::DEG2RAD;
+}
