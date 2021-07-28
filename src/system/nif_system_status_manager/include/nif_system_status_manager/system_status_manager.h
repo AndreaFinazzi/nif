e0bf3dbefc9e484b25f38cf3cad2d74286a8d04c
Andrea Finazzi <af.andreafinazzi@gmail.com>
2021-07-28 13:33:15 +0200

    --symlink-install removed from build options

---
+++ src/system/nif_system_status_manager/include/nif_system_status_manager/system_status_manager.h
@@ -0,0 +1,10 @@
+//
+// Created by usrg on 7/28/21.
+//
+
+#ifndef ROS2MASTER_SYSTEM_STATUS_MANAGER_H
+#define ROS2MASTER_SYSTEM_STATUS_MANAGER_H
+
+class system_status_manager {};
+
+#endif // ROS2MASTER_SYSTEM_STATUS_MANAGER_H
