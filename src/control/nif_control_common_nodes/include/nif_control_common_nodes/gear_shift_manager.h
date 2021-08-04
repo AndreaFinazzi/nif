d973ce903b6c86dec670390547ee0ec822eb3205
chanyoungjung <hy910915@kaist.ac.kr>
2021-08-04 16:36:10 -0400

    Add gearShift holding functionality

--- src/control/nif_control_common_nodes/include/nif_control_common_nodes/gear_shift_manager.h
+++ src/control/nif_control_common_nodes/include/nif_control_common_nodes/gear_shift_manager.h
@@ -10,10 +10,15 @@
 
 #include "nif_common/vehicle_model.h"
 #include <algorithm>
+#include <chrono>
+#include <ctime>
 #include <iostream>
+#include <sys/time.h>
 
 class GearShiftManager {
 private:
+  int max_gear;
+  int min_gear;
   int current_gear;
   int current_rpm;
   double current_vel;
@@ -21,29 +26,40 @@ private:
   double desired_vel;      // from the longitudinal controller, velocity command
   bool is_upshift_require; // if gear upshifting is needed, set to True
   bool is_downshift_require; // if gear downshifting is needed, set to True
-  bool is_hold_gear_require;
+  bool is_stay_gear_require;
   double rpm_gain_for_upshift =
       0.9; // It must be lower or equal to 1.0 (percentage)
   double rpm_gain_for_downshift =
       1.1; // It must be greater or equal to 1.0 (percentage)
+  double last_gearshift_timestamp_sec = 0.0;
+  int last_desired_gear;
+  bool hold_gearshift_flg = false;
+
 public:
   void setRpmGainForUpshift(double rpmGainForUpshift);
   void setRpmGainForDownshift(double rpmGainForDownshift);
 
 public:
+  int getMaxGear() const;
+  void setMaxGear(int maxGear);
+  int getMinGear() const;
+  void setMinGear(int minGear);
   void setCurrentRpm(int currentRpm);
   void setCurrentVel(double currentVel);
   void setDesiredVel(double desiredVel);
   void setCurrentGear(int currentGear);
   bool isUpshiftRequire() const;
   bool isDownshiftRequire() const;
-  bool isHoldGearRequire() const;
+  bool isStayGearRequire() const;
 
   /**
    * returns desired gear position.
    * @return int
    */
-  int getDesiredGear(double desiredVel, double currentVel, int currentGear);
+  int getDesiredGear(double desiredVel,
+                     double currentVel,
+                     int currentGear,
+                     int currentRpm);
 
   // Document from the teams' gitlab
   // https://docs.google.com/document/d/1gJYag8tVhtnSl3MjWvsVMBEfaR9WXndrybvJtXeLbU8/edit#
@@ -54,7 +70,11 @@ public:
 
 public:
   GearShiftManager();
+  GearShiftManager(int max_gear, int min_gear);
   ~GearShiftManager();
+
+  void init();
+  void init(int max_gear, int min_gear);
 };
 
 #endif // ROS2MASTER_GEAR_SHIFT_MANAGER_H
--- src/control/nif_control_common_nodes/src/gear_shift_manager.cpp
+++ src/control/nif_control_common_nodes/src/gear_shift_manager.cpp
@@ -4,19 +4,33 @@
 #include "nif_control_common_nodes/gear_shift_manager.h"
 
 using namespace nif::common::vehicle_param;
-GearShiftManager::GearShiftManager(/* args */) {}
+using namespace std::chrono;
+
+GearShiftManager::GearShiftManager(/* args */) {
+  this->init();
+}
+void GearShiftManager::init() {
+  this->max_gear = gear_box::MAX_GEAR_POS;
+  this->min_gear = gear_box::MIN_GEAR_FOR_DOWNSHIFT_POS;
+}
+GearShiftManager::GearShiftManager(int maxGear, int minGear) {
+  this->init(maxGear, minGear);
+}
+void GearShiftManager::init(int maxGear, int minGear) {
+  this->max_gear = maxGear;
+  this->min_gear = minGear;
+}
 
 GearShiftManager::~GearShiftManager() {}
 
-void GearShiftManager::setCurrentGear(int currentGear) {
-  current_gear = currentGear;
+bool GearShiftManager::isUpshiftRequire() const {
+  return is_upshift_require;
 }
-bool GearShiftManager::isUpshiftRequire() const { return is_upshift_require; }
 bool GearShiftManager::isDownshiftRequire() const {
   return is_downshift_require;
 }
-bool GearShiftManager::isHoldGearRequire() const {
-  return is_hold_gear_require;
+bool GearShiftManager::isStayGearRequire() const {
+  return is_stay_gear_require;
 }
 void GearShiftManager::setCurrentVel(double currentVel) {
   current_vel = currentVel;
@@ -24,57 +38,115 @@ void GearShiftManager::setCurrentVel(double currentVel) {
 void GearShiftManager::setDesiredVel(double desiredVel) {
   desired_vel = desiredVel;
 }
-int GearShiftManager::getDesiredGear(double desiredVel, double currentVel,
-                                     int currentGear) {
-  desired_vel = desiredVel;
-  current_vel = currentVel;
+void GearShiftManager::setCurrentGear(int currentGear) {
   current_gear = currentGear;
+}
+void GearShiftManager::setCurrentRpm(int currentRpm) {
+  current_rpm = currentRpm;
+}
+int GearShiftManager::getDesiredGear(double desiredVel,
+                                     double currentVel,
+                                     int currentGear,
+                                     int currentRpm) {
+  this->setDesiredVel(desiredVel);
+  this->setCurrentVel(currentVel);
+  this->setCurrentGear(currentGear);
+  this->setCurrentRpm(currentRpm);
 
-  // TODO: calculate desired gear position
+  auto current_time = std::chrono::system_clock::now();
+  auto duration_in_seconds =
+      std::chrono::duration<double>(current_time.time_since_epoch());
+  double current_timestamp_sec = duration_in_seconds.count();
+
   if (desiredVel > currentVel) {
     //    considering up shift (acceleration required)
-    if (current_rpm > gear_box::safe_upshift::max_rpm_vector[current_gear] *
-                          rpm_gain_for_upshift &&
-        current_gear != gear_box::MAX_GEAR_POS) {
-      //      need up shifting
-      is_upshift_require = true;
-      is_downshift_require = false;
-      is_hold_gear_require = false;
-      desired_gear = current_gear + 1;
+    if(abs(current_timestamp_sec-last_gearshift_timestamp_sec) < 0.5 &&
+        hold_gearshift_flg == true) {
+      if(last_desired_gear > current_gear){
+        is_upshift_require = true;
+        is_downshift_require = false;
+        is_stay_gear_require = false;
+      } else {
+        is_upshift_require = false;
+        is_downshift_require = false;
+        is_stay_gear_require = true;
+      }
+      return last_desired_gear;
     } else {
-      //      keep the current gear
-      is_upshift_require = false;
-      is_downshift_require = false;
-      is_hold_gear_require = true;
-      desired_gear = current_gear;
+      hold_gearshift_flg = false;
+      if (current_rpm >
+              gear_box::safe_upshift::max_rpm_vector[current_gear] *
+                  rpm_gain_for_upshift &&
+          current_gear != this->max_gear) {
+        //      need up shifting
+        is_upshift_require = true;
+        is_downshift_require = false;
+        is_stay_gear_require = false;
+        desired_gear = current_gear + 1;
+        //      store last gearshift info
+        hold_gearshift_flg = true;
+        last_desired_gear = desired_gear;
+        auto current_time = std::chrono::system_clock::now();
+        auto duration_in_seconds =
+            std::chrono::duration<double>(current_time.time_since_epoch());
+        last_gearshift_timestamp_sec = duration_in_seconds.count();
+      } else {
+        //      keep the current gear
+        is_upshift_require = false;
+        is_downshift_require = false;
+        is_stay_gear_require = true;
+        desired_gear = current_gear;
+        hold_gearshift_flg = false;
+      }
     }
   } else {
     //    considering downshift (deceleration required)
-    if (current_rpm <
-            gear_box::safe_downshift::reference_rpm_vector[current_gear] *
-                rpm_gain_for_downshift &&
-        current_gear != gear_box::MIN_GEAR_FOR_DOWNSHIFT_POS) {
-      //      good to go down shifting
-      is_upshift_require = false;
-      is_downshift_require = true;
-      is_hold_gear_require = false;
-      desired_gear = current_gear - 1;
+    if(abs(current_timestamp_sec-last_gearshift_timestamp_sec) < 0.5 &&
+       hold_gearshift_flg == true) {
+      if(last_desired_gear > current_gear){
+        is_upshift_require = true;
+        is_downshift_require = false;
+        is_stay_gear_require = false;
+      } else {
+        is_upshift_require = false;
+        is_downshift_require = false;
+        is_stay_gear_require = true;
+      }
+      return last_desired_gear;
     } else {
-      //      keep the current gear
-      is_upshift_require = false;
-      is_downshift_require = false;
-      is_hold_gear_require = true;
-      desired_gear = current_gear;
+      hold_gearshift_flg = false;
+      if (current_rpm <
+              gear_box::safe_downshift::reference_rpm_vector[current_gear] *
+                  rpm_gain_for_downshift &&
+          current_gear != this->min_gear) {
+        //      good to go down shifting
+        is_upshift_require = false;
+        is_downshift_require = true;
+        is_stay_gear_require = false;
+        desired_gear = current_gear - 1;
+        hold_gearshift_flg = true;
+        last_desired_gear = desired_gear;
+        //      store last gearshift info
+        auto current_time = std::chrono::system_clock::now();
+        auto duration_in_seconds =
+            std::chrono::duration<double>(current_time.time_since_epoch());
+        last_gearshift_timestamp_sec = duration_in_seconds.count();
+      } else {
+        //      keep the current gear
+        is_upshift_require = false;
+        is_downshift_require = false;
+        is_stay_gear_require = true;
+        desired_gear = current_gear;
+        hold_gearshift_flg = false;
+      }
     }
   }
 
   desired_gear = std::clamp(desired_gear,
                             nif::common::vehicle_param::gear_box::MIN_GEAR_POS,
                             nif::common::vehicle_param::gear_box::MAX_GEAR_POS);
+
   return desired_gear;
-}
-void GearShiftManager::setCurrentRpm(int currentRpm) {
-  current_rpm = currentRpm;
 }
 /**
  * It must be lower or equal to 1.0 (percentage)
@@ -90,4 +162,16 @@ void GearShiftManager::setRpmGainForUpshift(double rpmGainForUpshift) {
  */
 void GearShiftManager::setRpmGainForDownshift(double rpmGainForDownshift) {
   rpm_gain_for_downshift = rpmGainForDownshift;
+}
+int GearShiftManager::getMaxGear() const {
+  return max_gear;
+}
+void GearShiftManager::setMaxGear(int maxGear) {
+  max_gear = maxGear;
+}
+int GearShiftManager::getMinGear() const {
+  return min_gear;
+}
+void GearShiftManager::setMinGear(int minGear) {
+  min_gear = minGear;
 }
