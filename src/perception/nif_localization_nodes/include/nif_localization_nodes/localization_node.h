ac1b90cfc0fca5245ea24c4e2816d67863ac7aea
Seungwook <seungwook1024@kaist.ac.kr>
2021-08-09 11:37:12 +0900

    Made changes to avoid future merge conflicts

--- src/perception/nif_localization_nodes/include/nif_localization_nodes/localization_node.h
+++ src/perception/nif_localization_nodes/include/nif_localization_nodes/localization_node.h
@@ -32,9 +32,9 @@ using namespace std::chrono_literals;
 
 class LocalizationNode : public nif::common::IBaseNode {
 public:
-  explicit LocalizationNode(const std::string& node_name_);
+  explicit LocalizationNode(const std::string &node_name_);
   LocalizationNode(
-      const std::string& node_name_,
+      const std::string &node_name_,
       const std::shared_ptr<LocalizationMinimal> localization_algorithm_ptr);
   ~LocalizationNode() {}
 
@@ -42,23 +42,23 @@ private:
   LocalizationNode();
   void timer_callback();
   void syncGPSCallback(
-      const novatel_gps_msgs::msg::Inspva::ConstSharedPtr& gps_horizontal_,
-      const novatel_gps_msgs::msg::Inspva::ConstSharedPtr& gps_vertical_);
+      const novatel_gps_msgs::msg::Inspva::ConstSharedPtr &gps_horizontal_,
+      const novatel_gps_msgs::msg::Inspva::ConstSharedPtr &gps_vertical_);
 
-    void gpsHorizontalCallback(
-            const novatel_gps_msgs::msg::Inspva::SharedPtr gps_horizontal_ptr_);
+  void gpsHorizontalCallback(
+      const novatel_gps_msgs::msg::Inspva::SharedPtr gps_horizontal_ptr_);
 
-    void publishTransformStamped();
+  void publishTransformStamped();
 
-    // TODO: not used function. @Andrea told that these functions should be
+  // TODO: not used function. @Andrea told that these functions should be
   // fixed or removed
   void initParameters() {}
   void getParameters() {}
 
   geometry_msgs::msg::TransformStamped transform_stamped;
 
-  void topGPSCallback(const novatel_gps_msgs::msg::Inspva::SharedPtr& gps_);
-  void bottomGPSCallback(const novatel_gps_msgs::msg::Inspva::SharedPtr& gps_);
+  void topGPSCallback(const novatel_gps_msgs::msg::Inspva::SharedPtr &gps_);
+  void bottomGPSCallback(const novatel_gps_msgs::msg::Inspva::SharedPtr &gps_);
 
   std::shared_ptr<LocalizationMinimal> m_localization_algorithm_ptr;
   nav_msgs::msg::Odometry m_veh_odom;
@@ -69,14 +69,12 @@ private:
   message_filters::Subscriber<novatel_gps_msgs::msg::Inspva>
       m_gps_vertical_subscriber;
 
-  std::shared_ptr<
-      message_filters::TimeSynchronizer<novatel_gps_msgs::msg::Inspva,
-                                        novatel_gps_msgs::msg::Inspva>>
+  std::shared_ptr<message_filters::TimeSynchronizer<
+      novatel_gps_msgs::msg::Inspva, novatel_gps_msgs::msg::Inspva>>
       m_gps_sync_ptr;
 
   rclcpp::Subscription<novatel_gps_msgs::msg::Inspva>::SharedPtr
-            m_gps_horizontal_sub;
-
+      m_gps_horizontal_sub;
 
   rclcpp::TimerBase::SharedPtr m_timer;
 };
