46f8f2a17e769a0c25482f35bd3717fbd2b3e8a7
Andrea Finazzi <af.andreafinazzi@gmail.com>
2021-08-13 10:43:09 +0900

    clion in separate volume

--- nif_image_build.sh
+++ nif_image_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-docker build docker/ -t usrg/nifde:master
+docker build docker/ -t usrg/nifde:master --build-arg GH_TOKEN=$CR_PAT
 
 docker tag usrg/nifde:master ghcr.io/andreafinazzi/ade:master
 docker push ghcr.io/andreafinazzi/ade:master
