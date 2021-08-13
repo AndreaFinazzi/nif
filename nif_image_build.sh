9961f30c7c752b8d24bdf9828ead8d8a8dd05441
Andrea Finazzi <af.andreafinazzi@gmail.com>
2021-08-13 09:41:57 +0900

    dependency management moved to dokcer image

--- nif_image_build.sh
+++ nif_image_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-docker build docker/ -t usrg/nifde:ssh
+docker build docker/ -t usrg/nifde:ssh --build-arg GH_TOKEN=$CR_PAT
 
 docker tag usrg/nifde:ssh ghcr.io/andreafinazzi/ade:ssh
 docker push ghcr.io/andreafinazzi/ade:ssh
