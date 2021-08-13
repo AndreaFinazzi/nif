895684f244ad9f0c86472e16fd81f06a9ea0a317
Andrea Finazzi <af.andreafinazzi@gmail.com>
2021-08-13 11:50:34 +0900

    CR_PAT to GH_TOKEN

--- nif_image_build.sh
+++ nif_image_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-docker build docker/ -t usrg/nifde:master --build-arg GH_TOKEN=$CR_PAT
+docker build docker/ -t usrg/nifde:master --build-arg GH_TOKEN=$GH_TOKEN
 
 docker tag usrg/nifde:master ghcr.io/andreafinazzi/ade:master
 docker push ghcr.io/andreafinazzi/ade:master
