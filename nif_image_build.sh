86b7d831c0341988e509d76c36ce19f87074b1e9
Andrea Finazzi <af.andreafinazzi@gmail.com>
2021-08-13 09:55:07 +0900

    ade tag from :ssh to :master (for CI operations compatibility)

--- nif_image_build.sh
+++ nif_image_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-docker build docker/ -t usrg/nifde:ssh
+docker build docker/ -t usrg/nifde:master
 
-docker tag usrg/nifde:ssh ghcr.io/andreafinazzi/ade:ssh
-docker push ghcr.io/andreafinazzi/ade:ssh
+docker tag usrg/nifde:master ghcr.io/andreafinazzi/ade:master
+docker push ghcr.io/andreafinazzi/ade:master
