9a74d77d27b26beaedfdf48e1ecd50a1929343a1
hyunki <hynkis@kaist.ac.kr>
2021-10-01 21:43:57 -0400

    add Traction control

--- src/control/nif_accel_control_nodes/include/TractionABS.hpp
+++ src/control/nif_accel_control_nodes/include/TractionABS.hpp
@@ -23,33 +23,51 @@ namespace control {
 class TractionABS {
 public:
   /// @brief Constructor
+  /// @param[in] traction_enabled
+  /// @param[in] traction_throttle_cmd_thres
+  /// @param[in] traction_factor
+  /// @param[in] traction_rate
   /// @param[in] ABS_enabled
-  /// @param[in] velocity_thres_mps
-  /// @param[in] brake_cmd_thres
-  /// @param[in] sigma_thres
+  /// @param[in] ABS_brake_cmd_thres
   /// @param[in] ABS_factor
-  /// @param[in] control_rate
   /// @param[in] ABS_rate
+  /// @param[in] velocity_thres_mps
+  /// @param[in] sigma_thres
+  /// @param[in] control_rate
 
-  TractionABS(const bool &ABS_enabled = true,
+  TractionABS(const bool &traction_enabled = true,
+              const double &traction_throttle_cmd_thres = 0.5,
+              const double &traction_factor = 0.5,
+              const double &traction_rate = 15.0,
+              const bool &ABS_enabled = true,
+              const double &ABS_brake_cmd_thres = 0.5,
+              const double &ABS_factor = 0.5, const double &ABS_rate = 15.0,
               const double &velocity_thres_mps = 27.78,
-              const double &brake_cmd_thres = 0.5,
-              const double &sigma_thres = 0.06, const double &ABS_factor = 0.5,
-              const double &control_rate = 100., const double &ABS_rate = 15.);
+              const double &sigma_thres = 0.06,
+              const double &control_rate = 100.);
 
-  /// @brief ABS parameters
-  int cnt = 0;
+  /// @brief Parameters
+  int cnt_traction = 0;
+  int cnt_ABS = 0;
 
-  double ABS_control(double brake_cmd, double current_velocity, double sigma);
+  double tractionControl(double throttle_cmd, double current_velocity,
+                         double sigma);
+  double ABSControl(double brake_cmd, double current_velocity, double sigma);
 
 private:
+  bool traction_enabled_ = true;
+  double traction_throttle_cmd_thres_ = 0.5;
+  double traction_factor_ = 0.5;
+  double traction_rate_ = 15.;
+
   bool ABS_enabled_ = true;
-  double velocity_thres_mps_ = 27.78;
-  double brake_cmd_thres_ = 0.5;
-  double sigma_thres_ = 0.06;
+  double ABS_brake_cmd_thres_ = 0.5;
   double ABS_factor_ = 0.5;
-  double control_rate_ = 100.0;
   double ABS_rate_ = 15.;
+
+  double velocity_thres_mps_ = 27.78;
+  double sigma_thres_ = 0.06;
+  double control_rate_ = 100.0;
 
 }; // End class TractionABS
 
--- src/control/nif_accel_control_nodes/src/TractionABS.cpp
+++ src/control/nif_accel_control_nodes/src/TractionABS.cpp
@@ -18,23 +18,72 @@
 #include <iostream>
 
 namespace control {
-TractionABS::TractionABS(const bool &ABS_enabled,
+TractionABS::TractionABS(const bool &traction_enabled,
+                         const double &traction_throttle_cmd_thres,
+                         const double &traction_factor,
+                         const double &traction_rate, const bool &ABS_enabled,
+                         const double &ABS_brake_cmd_thres,
+                         const double &ABS_factor, const double &ABS_rate,
                          const double &velocity_thres_mps,
-                         const double &brake_cmd_thres,
-                         const double &sigma_thres, const double &ABS_factor,
-                         const double &control_rate, const double &ABS_rate) {
+                         const double &sigma_thres,
+                         const double &control_rate) {
   // Set control parameters
+  // - enabling Traction/ABS
+  traction_enabled_ = traction_enabled;
   ABS_enabled_ = ABS_enabled;
-  velocity_thres_mps_ = velocity_thres_mps;
-  brake_cmd_thres_ = brake_cmd_thres;
-  sigma_thres_ = sigma_thres;
+  // - throttle/brake cmd threshold for ABS activation. 0.5
+  traction_throttle_cmd_thres_ = traction_throttle_cmd_thres;
+  ABS_brake_cmd_thres_ = ABS_brake_cmd_thres;
+  // - decreasing throttle/brake cmd factor. 0.5
+  traction_factor_ = traction_factor;
   ABS_factor_ = ABS_factor;
-  control_rate_ = control_rate;
+  // - control rate of Traction/ABS activagion
+  traction_rate_ = traction_rate;
   ABS_rate_ = ABS_rate;
+  // - velocity threshold for ABS activation.
+  velocity_thres_mps_ = velocity_thres_mps;
+  // - slip ratio threshold. 0.06
+  sigma_thres_ = sigma_thres;
+  // - control rate of low-level accel controller
+  control_rate_ = control_rate;
 }
 
-double TractionABS::ABS_control(double brake_cmd, double current_velocity,
-                                double sigma) {
+double TractionABS::tractionControl(double throttle_cmd,
+                                    double current_velocity, double sigma) {
+  // Init
+  double throttle_cmd_new = throttle_cmd;
+  // Tire slip ratio
+  // - assume 'vx == free wheel speed'
+  // - sigma = (braking_wheelspeed - rolling_wheelspeed) /
+
+  // Traction Control activation condition
+  bool active_traction = (traction_enabled_ == true) &&
+                         (current_velocity > velocity_thres_mps_) &&
+                         (throttle_cmd > traction_throttle_cmd_thres_) &&
+                         (std::abs(sigma) > sigma_thres_);
+  // Traction Control algorithm
+  if (active_traction) {
+    // check count for Traction Control activation timing;
+    int cnt_condition =
+        cnt_traction % int(int(control_rate_) % int(traction_rate_));
+    if (cnt_condition == 0) {
+      // Release brake command
+      throttle_cmd_new = traction_factor_ * throttle_cmd;
+      std::cout << "Traction Control is activated!!!!!!!!!!!!!!!!!!!!!!"
+                << std::endl;
+    }
+  }
+
+  // Traction Control counter
+  cnt_traction++;
+  if (cnt_traction > 255)
+    cnt_traction = 0;
+
+  return throttle_cmd_new;
+}
+
+double TractionABS::ABSControl(double brake_cmd, double current_velocity,
+                               double sigma) {
   // Init
   double brake_cmd_new = brake_cmd;
   // Tire slip ratio
@@ -44,20 +93,22 @@ double TractionABS::ABS_control(double brake_cmd, double current_velocity,
   // ABS activation condition
   bool active_ABS =
       (ABS_enabled_ == true) && (current_velocity > velocity_thres_mps_) &&
-      (brake_cmd > brake_cmd_thres_) && (std::abs(sigma) > sigma_thres_);
+      (brake_cmd > ABS_brake_cmd_thres_) && (std::abs(sigma) > sigma_thres_);
   // ABS algorithm
   if (active_ABS) {
     // check count for ABS activation timing;
-    if (cnt % int(int(control_rate_) % int(ABS_rate_))) {
+    int cnt_condition = cnt_ABS % int(int(control_rate_) % int(ABS_rate_));
+    if (cnt_condition == 0) {
       // Release brake command
       brake_cmd_new = ABS_factor_ * brake_cmd;
+      std::cout << "ABS is activated!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
     }
   }
 
   // ABS counter
-  cnt++;
-  if (cnt > 255)
-    cnt = 0;
+  cnt_ABS++;
+  if (cnt_ABS > 255)
+    cnt_ABS = 0;
 
   return brake_cmd_new;
 }
