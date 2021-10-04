fa37b64039ea3db5978065ef1a8870c913581771
chanyoungjung <hy910915@kaist.ac.kr>
2021-10-03 22:39:51 -0400

    IMS graph and pit-in waypoint are updated. Also, Graph with mission bbox visualization scripts for each track are added inside of the nif_graph_planner/lib folder.

--- src/planning/nif_multilayer_planning_nodes/lib/GraphBasedLocalTrajectoryPlanner/ims_visualization.py
+++ src/planning/nif_multilayer_planning_nodes/lib/GraphBasedLocalTrajectoryPlanner/ims_visualization.py
@@ -10,6 +10,8 @@ import pickle
 import sys
 import logging
 import graph_ltpl
+import yaml
+import matplotlib.patches as patches
 
 ax = plt.subplot(1,1,1)
 
@@ -135,13 +137,66 @@ if __name__ == "__main__":
     TRACK_NAME = 'IMS'
 
     dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..'))
+    dir_two_up = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..', '..', '..'))
     pit_wpt = os.path.join(dir,'params',TRACK_NAME,'pit_lane.csv',)
     raceline = os.path.join(dir,'inputs/traj_ltpl_cl',TRACK_NAME,'traj_race_cl.csv',)
     graph = os.path.join(dir,'inputs/track_offline_graphs',TRACK_NAME,'stored_graph.pckl',)
 
+    mission_yaml_file = os.path.join(dir_two_up,'system','nif_mission_manager_nodes','config', 'transitions.ims.yaml')
+    with open(mission_yaml_file) as f:
+        mission_bbox = yaml.safe_load(f)
+
+    pit_in_box = None
+    pit_standby_box = None
+    pit_out_box = None
+    pit_init_box = None
+
+    for i, mission_code_block in enumerate(mission_bbox.get("missions")):
+        if mission_code_block.get("mission_code") == 60: # pit-in
+            pit_in_box = mission_code_block.get("activation_area").get("bboxes")[0]
+            print(pit_in_box)
+
+        elif mission_code_block.get("mission_code") == 65: # pit-standby
+            pit_standby_box = mission_code_block.get("activation_area").get("bboxes")[0]
+            print(pit_standby_box)
+
+        elif mission_code_block.get("mission_code") == 75: # pit-out
+            pit_out_box = mission_code_block.get("activation_area").get("bboxes")[0]
+            print(pit_out_box)
+
+        elif mission_code_block.get("mission_code") == 65000: # pit-init
+            pit_init_box = mission_code_block.get("activation_area").get("bboxes")[0]
+            print(pit_init_box)
+
+    pit_in_box = patches.Rectangle((pit_in_box[0], pit_in_box[1]), # x_min, y_min
+                            pit_in_box[2] - pit_in_box[0], # x-wise width
+                            pit_in_box[3] - pit_in_box[1], # y-wise height
+                            linewidth=1, edgecolor='r', fc=(1,0,0,0.3), label='pit_in_box [from mission yaml]')
+
+    pit_standby_box = patches.Rectangle((pit_standby_box[0], pit_standby_box[1]), # x_min, y_min
+                            pit_standby_box[2] - pit_standby_box[0], # x-wise width
+                            pit_standby_box[3] - pit_standby_box[1], # y-wise height
+                            linewidth=1, edgecolor='g', fc=(0,1,0,0.3), label='pit_standby_box [from mission yaml]')
+
+    pit_out_box = patches.Rectangle((pit_out_box[0], pit_out_box[1]), # x_min, y_min
+                            pit_out_box[2] - pit_out_box[0], # x-wise width
+                            pit_out_box[3] - pit_out_box[1], # y-wise height
+                            linewidth=1, edgecolor='b', fc=(0,0,1,0.3), label='pit_out_box [from mission yaml]')
+
+    pit_init_box = patches.Rectangle((pit_init_box[0], pit_init_box[1]), # x_min, y_min
+                            pit_init_box[2] - pit_init_box[0], # x-wise width
+                            pit_init_box[3] - pit_init_box[1], # y-wise height
+                            linewidth=1, edgecolor='k', fc=(0.8,0.4,0,0.3), label='pit_init_box [from mission yaml]')
+
+    ax.add_patch(pit_in_box)
+    ax.add_patch(pit_standby_box)
+    ax.add_patch(pit_out_box)
+    ax.add_patch(pit_init_box)
 
     WPTFileVisualizer(pit_wpt, "pit-in-entire")
     RaceLineFileVisualizer(raceline, "race-line")
     GraphVisualizer(graph, "graph_node")
+    plt.axis('equal')
+    plt.grid()
     plt.show()
 
