aecf3760c18f0f37dbd0796b6cdf18f8011b5f64
Andrea Finazzi <af.andreafinazzi@gmail.com>
2021-11-27 13:31:39 -0800

    avt_vimba camera config fixes. gettext-base to nif image apt packages

--- nif_image_build.sh
+++ nif_image_build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 export GL_TOKEN=$(cat .secret.gitlab) 
 export GH_TOKEN=$(cat .secret.github) 
-docker build docker/ -t usrg/nifde:${ROS_DISTRO} --build-arg GH_TOKEN=$GH_TOKEN --build-arg GL_TOKEN=$GL_TOKEN
+docker build docker/ -t usrg/nifde:$ROS_DISTRO --build-arg GH_TOKEN=$GH_TOKEN --build-arg GL_TOKEN=$GL_TOKEN
 
-docker tag usrg/nifde:${ROS_DISTRO} ghcr.io/andreafinazzi/nif:${ROS_DISTRO}
-docker push ghcr.io/andreafinazzi/nif:${ROS_DISTRO}
+docker tag usrg/nifde:$ROS_DISTRO ghcr.io/andreafinazzi/nif:$ROS_DISTRO
+docker push ghcr.io/andreafinazzi/nif:$ROS_DISTRO
