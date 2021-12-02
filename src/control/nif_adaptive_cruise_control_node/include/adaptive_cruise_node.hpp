697da6924b797b544047f26c7e9047306a858a9f
chanyoungjung <hy910915@gmail.com>
2021-12-02 14:56:07 -0500

    Fixed compile errors.

--- src/control/nif_adaptive_cruise_control_node/include/adaptive_cruise_node.hpp
+++ src/control/nif_adaptive_cruise_control_node/include/adaptive_cruise_node.hpp
@@ -5,51 +5,48 @@
 #ifndef IDM_BASED_ACC_NODE_H
 #define IDM_BASED_ACC_NODE_H
 
-#include <iostream>
 #include "idm_acc_lib/c++/IDM.hpp"
-#include <rclcpp/rclcpp.hpp>
-#include <nav_msgs/msg/odometry.hpp>
-#include <deep_orange_msgs/msg/pt_report.hpp>
-#include <raptor_dbw_msgs/msg/wheel_speed_report.hpp>
 #include "nif_common_nodes/i_base_node.h"
 #include "nif_msgs/msg/detected_object.hpp"
 #include "nif_msgs/msg/detected_object_array.hpp"
 #include <ament_index_cpp/get_package_share_directory.hpp>
+#include <deep_orange_msgs/msg/pt_report.hpp>
+#include <iostream>
+#include <nav_msgs/msg/odometry.hpp>
+#include <raptor_dbw_msgs/msg/wheel_speed_report.hpp>
+#include <rclcpp/rclcpp.hpp>
 
-namespace nif
-{
-    namespace control
-    {
-        class IDMACCNode : public nif::common::IBaseNode
-        {
+namespace nif {
+namespace control {
+class IDMACCNode : public nif::common::IBaseNode {
 
-        public:
-            explicit IDMACCNode(const std::string &node_name_);
-            IDMACCNode(const std::string &node_name_,
-                       const std::shared_ptr<IDM> idm_prt_);
-            ~IDMACCNode();
+public:
+  IDMACCNode(const std::string &node_name_);
+  //   IDMACCNode(const std::string &node_name_,
+  //              const std::shared_ptr<IDM> idm_prt_);
+  ~IDMACCNode() {}
 
-            void detectionCallback(const nif_msgs::msg::DetectedObjectArray::SharedPtr det_msg);
+  void detectionCallback(
+      const nif_msgs::msg::DetectedObjectArray::SharedPtr det_msg);
 
-        private:
-            /* data */
-            IDMACCNode();
-            double m_acc_cmd;
-            double m_veh_speed_mps;
+private:
+  /* data */
+  //   IDMACCNode();
+  double m_acc_cmd;
+  double m_veh_speed_mps;
 
-            std::string m_config_file;
+  std::string m_config_file;
 
-            nif_msgs::msg::DetectedObjectArray m_det_result;
-            nav_msgs::msg::Odometry m_ego_odom;
+  nif_msgs::msg::DetectedObjectArray m_det_result;
+  nav_msgs::msg::Odometry m_ego_odom;
 
-            std::shared_ptr<IDM>
-                m_idm_prt;
+  std::shared_ptr<IDM> m_idm_prt;
 
-            // detection result subscriber
-            rclcpp::Subscription<nif_msgs::msg::DetectedObjectArray>::SharedPtr
-                m_detection_subscriber;
-        };
-    }
-}
+  // detection result subscriber
+  rclcpp::Subscription<nif_msgs::msg::DetectedObjectArray>::SharedPtr
+      m_detection_subscriber;
+};
+} // namespace control
+} // namespace nif
 
 #endif // IDM_BASED_ACC_NODE_H
--- src/control/nif_adaptive_cruise_control_node/src/adaptive_cruise_node.cpp
+++ src/control/nif_adaptive_cruise_control_node/src/adaptive_cruise_node.cpp
@@ -7,27 +7,23 @@
 using namespace nif::control;
 
 IDMACCNode::IDMACCNode(const std::string &node_name_)
-    : IBaseNode(node_name_, common::NodeType::CONTROL)
-{
+    : IBaseNode(node_name_, common::NodeType::CONTROL) {
   std::string package_share_directory;
 
-  try
-  {
+  try {
     // This value shouldn't be used, it's as a backup if a config param is
     // missing.
     package_share_directory = ament_index_cpp::get_package_share_directory(
         "nif_adaptive_cruise_control_node");
-  }
-  catch (std::exception e)
-  {
+  } catch (std::exception e) {
     RCLCPP_FATAL(this->get_logger(), "Can't get package_share_directory");
   }
 
   m_detection_subscriber =
       this->create_subscription<nif_msgs::msg::DetectedObjectArray>(
           "/input_topic_name", nif::common::constants::QOS_CONTROL_CMD,
-          std::bind(&IDMACCNode::detectionCallback,
-                    this, std::placeholders::_1));
+          std::bind(&IDMACCNode::detectionCallback, this,
+                    std::placeholders::_1));
 
   this->declare_parameter("idm_acc_config_file", "idm_acc_config.yaml");
   m_config_file = this->get_parameter("idm_acc_config_file").as_string();
@@ -39,8 +35,8 @@ IDMACCNode::IDMACCNode(const std::string &node_name_)
   this->m_idm_prt = std::make_shared<IDM>(m_config_file);
 }
 
-void IDMACCNode::detectionCallback(const nif_msgs::msg::DetectedObjectArray::SharedPtr det_msg)
-{
+void IDMACCNode::detectionCallback(
+    const nif_msgs::msg::DetectedObjectArray::SharedPtr det_msg) {
   m_det_result = *det_msg;
 
   // Calc acc cmd
@@ -55,18 +51,19 @@ void IDMACCNode::detectionCallback(const nif_msgs::msg::DetectedObjectArray::Sha
   // 1. Convert opponent's position to the global
   // 2. Project to the ego's racing line to calculate the progress
   // 3. Calculate the progress gap
-  // * double check that the detection message is represent in body frame (if no, converte to the body frame)
+  // * double check that the detection message is represent in body frame (if
+  // no, converte to the body frame)
 
   int cipv_idx = 0;
 
-  if (this->hasEgoOdometry() && this->hasEgoPowertrainState() && (det_msg->objects.size() != 0))
-  {
+  if (this->hasEgoOdometry() && this->hasEgoPowertrainState() &&
+      (det_msg->objects.size() != 0)) {
     // NOTE : This is just for the test. Proecssing of CIPC data should be done.
-    m_idm_prt->calcAccel(m_veh_speed_mps, det_msg->objects[cipv_idx].pose.position.x, det_msg->objects[cipv_idx].velocity.linear.x);
+    m_idm_prt->calcAccel(m_veh_speed_mps,
+                         det_msg->objects[cipv_idx].pose.position.x,
+                         det_msg->objects[cipv_idx].velocity.linear.x);
     m_acc_cmd = m_idm_prt->getACCCmd();
-  }
-  else
-  {
+  } else {
     // abnormal situation
   }
 }
