b92e014d0f30685635393b44cd5c29ed7381ffc8
chanyoungjung <hy910915@kaist.ac.kr>
2021-12-10 02:15:33 -0500

    Updated nif_utils, mph2mps.

--- src/common/nif_utils/include/nif_utils/utils.h
+++ src/common/nif_utils/include/nif_utils/utils.h
@@ -19,97 +19,97 @@
 #include <rclcpp/parameter.hpp>
 #include <string>
 
-namespace nif
-{
-  namespace common
-  {
-    namespace utils
-    {
+namespace nif {
+namespace common {
+namespace utils {
 
-      namespace time
-      {
-        /**
-   * Time in seconds
-  **/
-        double secs(rclcpp::Time t);
+namespace time {
+/**
+ * Time in seconds
+ **/
+double secs(rclcpp::Time t);
 
-        /**
-   * Duration in seconds
-  **/
-        double secs(rclcpp::Duration t);
-      }
+/**
+ * Duration in seconds
+ **/
+double secs(rclcpp::Duration t);
+} // namespace time
 
-      namespace geometry
-      {
+namespace geometry {
 
-        const float MPH_TO_KPH_FACTOR = 1.60934;
-        const float KPH_TO_MPH_FACTOR = 0.621371;
+const float MPH_TO_KPH_FACTOR = 1.60934;
+const float KPH_TO_MPH_FACTOR = 0.621371;
 
-        /**
+/**
  * Calculate Euclidean distance between Point/Pose/PoseStamped
  * @param a Point/Pose/PoseStamped
  * @param b Point/Pose/PoseStamped
  * @return Euclidean distance between Point/Pose/PoseStamped
  */
-        double calEuclideanDistance(const geometry_msgs::msg::PoseStamped &a,
-                                    const geometry_msgs::msg::PoseStamped &b);
+double calEuclideanDistance(const geometry_msgs::msg::PoseStamped& a,
+                            const geometry_msgs::msg::PoseStamped& b);
 
-        /**
+/**
  * Calculate Euclidean distance between Point/Pose/PoseStamped
  * @param a Point/Pose/PoseStamped
  * @param b Point/Pose/PoseStamped
  * @return Euclidean distance between Point/Pose/PoseStamped
  */
-        double calEuclideanDistance(const geometry_msgs::msg::Pose &a,
-                                    const geometry_msgs::msg::Pose &b);
+double calEuclideanDistance(const geometry_msgs::msg::Pose& a,
+                            const geometry_msgs::msg::Pose& b);
 
-        /**
+/**
  * Calculate Euclidean distance between Point/Pose/PoseStamped
  * @param a Point/Pose/PoseStamped
  * @param b Point/Pose/PoseStamped
  * @return Euclidean distance between Point/Pose/PoseStamped
  */
-        double calEuclideanDistance(const geometry_msgs::msg::Point &a,
-                                    const geometry_msgs::msg::Point &b);
+double calEuclideanDistance(const geometry_msgs::msg::Point& a,
+                            const geometry_msgs::msg::Point& b);
 
-        //    TODO: fix static override problems
-        //    double calEuclideanDistance(const std::vector<double,double>& a, const
-        //    std::vector<double,double>& b);
+//    TODO: fix static override problems
+//    double calEuclideanDistance(const std::vector<double,double>& a, const
+//    std::vector<double,double>& b);
 
-        /**
+/**
  * Convert linear velocity from miles-per-hour to kilometers-per-hour
  * @param mph Linear velocity in miles-per-hour
  * @return Linear velocity in kilometers-per-hour
  */
-        double mph2kph(double mph);
+double mph2kph(double mph);
 
-        /**
+/**
  * Convert linear velocity from kilometers-per-hour to miles-per-hour
  * @param mph Linear velocity in kilometers-per-hour
  * @return Linear velocity in miles-per-hour
  */
-        double kph2mph(double kph);
+double kph2mph(double kph);
 
-      } // namespace geometry
+/**
+ * Convert linear velocity from mile-per-hour to miles-per-sec
+ * @param mph Linear velocity in mile-per-hour
+ * @return Linear velocity in miles-per-sec
+ */
+double mph2mps(double mph);
 
-      namespace io
-      {
-        //  TODO: define precisely (do we need this?)
-        std::vector<rclcpp::Parameter> &readConfig(std::string &file_name);
+} // namespace geometry
 
-      } // namespace io
+namespace io {
+//  TODO: define precisely (do we need this?)
+std::vector<rclcpp::Parameter>& readConfig(std::string& file_name);
 
-      // TODO: numeric is an awful name, needs something better.
-      namespace numeric
-      {
+} // namespace io
 
-        //    TODO: provide exaustive description of these two functions, along wth teir
-        //    params' description.
-        //     std::tuple<min_value, min_value_idx>
-        //    findMinValueNIdx(std::vector<double>& vec); std::tuple<max_value,
-        //    max_value_idx> findMaxValueNIdx(std::vector<double>& vec);
+// TODO: numeric is an awful name, needs something better.
+namespace numeric {
 
-        /**
+//    TODO: provide exaustive description of these two functions, along wth teir
+//    params' description.
+//     std::tuple<min_value, min_value_idx>
+//    findMinValueNIdx(std::vector<double>& vec); std::tuple<max_value,
+//    max_value_idx> findMaxValueNIdx(std::vector<double>& vec);
+
+/**
  * Clip the target value to min-max bounds.
  * @tparam T datatype must implement comparison operators
  * @param min Minimum value.
@@ -117,48 +117,42 @@ namespace nif
  * @param target Target value to be clipped.
  * @return Clipped value.
  */
-        template <typename T>
-        constexpr inline const T &clip(const T &min, const T &max, const T &target)
-        {
-          return std::max<T>(min, std::min<T>(target, max));
-        }
+template <typename T>
+constexpr inline const T& clip(const T& min, const T& max, const T& target) {
+  return std::max<T>(min, std::min<T>(target, max));
+}
 
-      } // namespace numeric
+} // namespace numeric
 
-      namespace coordination
-      {
+namespace coordination {
 
-        inline double quat2yaw(geometry_msgs::msg::Quaternion &data);
+inline double quat2yaw(geometry_msgs::msg::Quaternion& data);
 
-        inline double angle_wrap(double diff);
+inline double angle_wrap(double diff);
 
-        geometry_msgs::msg::PoseStamped
-        getPtBodytoGlobal(nav_msgs::msg::Odometry &current_pose_,
-                          geometry_msgs::msg::PoseStamped &point_in_body_);
-        geometry_msgs::msg::PoseStamped
-        getPtGlobaltoBody(nav_msgs::msg::Odometry &current_pose_,
-                          geometry_msgs::msg::PoseStamped &point_in_global_);
-        nav_msgs::msg::Path
-        getPathGlobaltoBody(
-            nav_msgs::msg::Odometry &current_pose_, nav_msgs::msg::Path &path_in_global_);
+geometry_msgs::msg::PoseStamped
+getPtBodytoGlobal(nav_msgs::msg::Odometry& current_pose_,
+                  geometry_msgs::msg::PoseStamped& point_in_body_);
+geometry_msgs::msg::PoseStamped
+getPtGlobaltoBody(nav_msgs::msg::Odometry& current_pose_,
+                  geometry_msgs::msg::PoseStamped& point_in_global_);
+nav_msgs::msg::Path getPathGlobaltoBody(nav_msgs::msg::Odometry& current_pose_,
+                                        nav_msgs::msg::Path& path_in_global_);
 
-      } // namespace coordination
+} // namespace coordination
 
-      namespace naming
-      {
+namespace naming {
 
-        inline const std::string getGlobalParamName(const std::string &param_name)
-        {
-          return nif::common::constants::parameters::DELIMITER +
-                 nif::common::constants::parameters::GLOBAL_PARAMETERS_NODE_NAME +
-                 nif::common::constants::parameters::DELIMITER +
-                 param_name;
-        }
+inline const std::string getGlobalParamName(const std::string& param_name) {
+  return nif::common::constants::parameters::DELIMITER +
+      nif::common::constants::parameters::GLOBAL_PARAMETERS_NODE_NAME +
+      nif::common::constants::parameters::DELIMITER + param_name;
+}
 
-      } // namespace naming
+} // namespace naming
 
-    } // namespace utils
-  }   // namespace common
+} // namespace utils
+} // namespace common
 } // namespace nif
 
 #endif // NIF_COMMON_NODES_UTILS_GEOMETRY_H
--- src/common/nif_utils/src/utils.cpp
+++ src/common/nif_utils/src/utils.cpp
@@ -11,57 +11,52 @@
 /**
  * Time in seconds
  **/
-double nif::common::utils::time::secs(rclcpp::Time t)
-{
+double nif::common::utils::time::secs(rclcpp::Time t) {
   return static_cast<double>(t.nanoseconds()) * 1e-9;
 }
 
 /**
  * Duration in seconds
  **/
-double nif::common::utils::time::secs(rclcpp::Duration t)
-{
+double nif::common::utils::time::secs(rclcpp::Duration t) {
   return static_cast<double>(t.nanoseconds()) * 1e-9;
 }
 
 double nif::common::utils::geometry::calEuclideanDistance(
-    const geometry_msgs::msg::Point &a, const geometry_msgs::msg::Point &b)
-{
+    const geometry_msgs::msg::Point& a, const geometry_msgs::msg::Point& b) {
   return sqrt(pow((a.x - b.x), 2) + pow((a.y - b.y), 2) + pow((a.z - b.z), 2));
 }
 
 double nif::common::utils::geometry::calEuclideanDistance(
-    const geometry_msgs::msg::Pose &a, const geometry_msgs::msg::Pose &b)
-{
+    const geometry_msgs::msg::Pose& a, const geometry_msgs::msg::Pose& b) {
   return calEuclideanDistance(a.position, b.position);
 }
 
 double nif::common::utils::geometry::calEuclideanDistance(
-    const geometry_msgs::msg::PoseStamped &a,
-    const geometry_msgs::msg::PoseStamped &b)
-{
+    const geometry_msgs::msg::PoseStamped& a,
+    const geometry_msgs::msg::PoseStamped& b) {
   return calEuclideanDistance(a.pose, b.pose);
 }
 
-double nif::common::utils::geometry::mph2kph(const double mph)
-{
+double nif::common::utils::geometry::mph2kph(const double mph) {
   return MPH_TO_KPH_FACTOR * mph;
 }
 
-double nif::common::utils::geometry::kph2mph(const double kph)
-{
+double nif::common::utils::geometry::kph2mph(const double kph) {
   return KPH_TO_MPH_FACTOR * kph;
 }
 
+double nif::common::utils::geometry::mph2mps(const double mph) {
+  return 0.44704 * mph;
+}
+
 inline double nif::common::utils::coordination::quat2yaw(
-    geometry_msgs::msg::Quaternion &data)
-{
+    geometry_msgs::msg::Quaternion& data) {
   return atan2(2 * (data.w * data.z + data.x * data.y),
                1 - 2 * (data.y * data.y + data.z * data.z));
 }
 
-inline double nif::common::utils::coordination::angle_wrap(double diff)
-{
+inline double nif::common::utils::coordination::angle_wrap(double diff) {
   diff = fmod(diff + nif::common::constants::numeric::PI,
               2 * nif::common::constants::numeric::PI);
   if (diff < 0)
@@ -72,18 +67,15 @@ inline double nif::common::utils::coordination::angle_wrap(double diff)
 
 geometry_msgs::msg::PoseStamped
 nif::common::utils::coordination::getPtBodytoGlobal(
-    nav_msgs::msg::Odometry &current_pose_,
-    geometry_msgs::msg::PoseStamped &point_in_body_)
-{
+    nav_msgs::msg::Odometry& current_pose_,
+    geometry_msgs::msg::PoseStamped& point_in_body_) {
   double current_yaw_rad = nif::common::utils::coordination::quat2yaw(
       current_pose_.pose.pose.orientation);
   geometry_msgs::msg::PoseStamped point_in_global;
-  point_in_global.pose.position.x =
-      current_pose_.pose.pose.position.x +
+  point_in_global.pose.position.x = current_pose_.pose.pose.position.x +
       point_in_body_.pose.position.x * cos(current_yaw_rad) -
       point_in_body_.pose.position.y * sin(current_yaw_rad);
-  point_in_global.pose.position.y =
-      current_pose_.pose.pose.position.y +
+  point_in_global.pose.position.y = current_pose_.pose.pose.position.y +
       point_in_body_.pose.position.x * sin(current_yaw_rad) +
       point_in_body_.pose.position.y * cos(current_yaw_rad);
   point_in_global.pose.position.z =
@@ -93,22 +85,23 @@ nif::common::utils::coordination::getPtBodytoGlobal(
 
 geometry_msgs::msg::PoseStamped
 nif::common::utils::coordination::getPtGlobaltoBody(
-    nav_msgs::msg::Odometry &current_pose_,
-    geometry_msgs::msg::PoseStamped &point_in_global_)
-{
+    nav_msgs::msg::Odometry& current_pose_,
+    geometry_msgs::msg::PoseStamped& point_in_global_) {
   double current_yaw_rad = nif::common::utils::coordination::quat2yaw(
       current_pose_.pose.pose.orientation);
   geometry_msgs::msg::PoseStamped point_in_body;
-  point_in_body.pose.position.x =
-      cos(-1 * current_yaw_rad) * (point_in_global_.pose.position.x -
-                                   current_pose_.pose.pose.position.x) -
-      sin(-1 * current_yaw_rad) * (point_in_global_.pose.position.y -
-                                   current_pose_.pose.pose.position.y);
-  point_in_body.pose.position.y =
-      sin(-1 * current_yaw_rad) * (point_in_global_.pose.position.x -
-                                   current_pose_.pose.pose.position.x) +
-      cos(-1 * current_yaw_rad) * (point_in_global_.pose.position.y -
-                                   current_pose_.pose.pose.position.y);
+  point_in_body.pose.position.x = cos(-1 * current_yaw_rad) *
+          (point_in_global_.pose.position.x -
+           current_pose_.pose.pose.position.x) -
+      sin(-1 * current_yaw_rad) *
+          (point_in_global_.pose.position.y -
+           current_pose_.pose.pose.position.y);
+  point_in_body.pose.position.y = sin(-1 * current_yaw_rad) *
+          (point_in_global_.pose.position.x -
+           current_pose_.pose.pose.position.x) +
+      cos(-1 * current_yaw_rad) *
+          (point_in_global_.pose.position.y -
+           current_pose_.pose.pose.position.y);
   point_in_body.pose.position.z =
       point_in_global_.pose.position.z - current_pose_.pose.pose.position.z;
 
@@ -123,38 +116,39 @@ nif::common::utils::coordination::getPtGlobaltoBody(
   return point_in_body;
 }
 
-nav_msgs::msg::Path
-nif::common::utils::coordination::getPathGlobaltoBody(
-    nav_msgs::msg::Odometry &current_pose_,
-    nav_msgs::msg::Path &path_in_global_)
-{
+nav_msgs::msg::Path nif::common::utils::coordination::getPathGlobaltoBody(
+    nav_msgs::msg::Odometry& current_pose_,
+    nav_msgs::msg::Path& path_in_global_) {
   double current_yaw_rad = nif::common::utils::coordination::quat2yaw(
       current_pose_.pose.pose.orientation);
 
   nav_msgs::msg::Path path_in_body;
-  for (int i = 0; i < path_in_global_.poses.size(); i++)
-  {
+  for (int i = 0; i < path_in_global_.poses.size(); i++) {
     geometry_msgs::msg::PoseStamped point_in_body;
-    point_in_body.pose.position.x =
-        cos(-1 * current_yaw_rad) * (path_in_global_.poses[i].pose.position.x -
-                                     current_pose_.pose.pose.position.x) -
-        sin(-1 * current_yaw_rad) * (path_in_global_.poses[i].pose.position.y -
-                                     current_pose_.pose.pose.position.y);
-    point_in_body.pose.position.y =
-        sin(-1 * current_yaw_rad) * (path_in_global_.poses[i].pose.position.x -
-                                     current_pose_.pose.pose.position.x) +
-        cos(-1 * current_yaw_rad) * (path_in_global_.poses[i].pose.position.y -
-                                     current_pose_.pose.pose.position.y);
-    point_in_body.pose.position.z =
-        path_in_global_.poses[i].pose.position.z - current_pose_.pose.pose.position.z;
+    point_in_body.pose.position.x = cos(-1 * current_yaw_rad) *
+            (path_in_global_.poses[i].pose.position.x -
+             current_pose_.pose.pose.position.x) -
+        sin(-1 * current_yaw_rad) *
+            (path_in_global_.poses[i].pose.position.y -
+             current_pose_.pose.pose.position.y);
+    point_in_body.pose.position.y = sin(-1 * current_yaw_rad) *
+            (path_in_global_.poses[i].pose.position.x -
+             current_pose_.pose.pose.position.x) +
+        cos(-1 * current_yaw_rad) *
+            (path_in_global_.poses[i].pose.position.y -
+             current_pose_.pose.pose.position.y);
+    point_in_body.pose.position.z = path_in_global_.poses[i].pose.position.z -
+        current_pose_.pose.pose.position.z;
 
     double target_yaw = nif::common::utils::coordination::quat2yaw(
         path_in_global_.poses[i].pose.orientation);
 
     point_in_body.pose.orientation.x = 0.0;
     point_in_body.pose.orientation.y = 0.0;
-    point_in_body.pose.orientation.z = sin((target_yaw - current_yaw_rad) / 2.0);
-    point_in_body.pose.orientation.w = cos((target_yaw - current_yaw_rad) / 2.0);
+    point_in_body.pose.orientation.z =
+        sin((target_yaw - current_yaw_rad) / 2.0);
+    point_in_body.pose.orientation.w =
+        cos((target_yaw - current_yaw_rad) / 2.0);
     path_in_body.poses.push_back(point_in_body);
   }
 
