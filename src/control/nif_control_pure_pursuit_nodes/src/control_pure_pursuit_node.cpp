a16dcecef14923a3853f5168a597b8c1874fd8de
chanyoungjung <hy910915@kaist.ac.kr>
2021-12-22 20:08:31 -0500

    Formatted.

--- src/control/nif_control_pure_pursuit_nodes/src/control_pure_pursuit_node.cpp
+++ src/control/nif_control_pure_pursuit_nodes/src/control_pure_pursuit_node.cpp
@@ -7,8 +7,9 @@ ControlPurePursuitNode::ControlPurePursuitNode(const std::string &node_name)
     : IControllerNode(node_name) {
 
   // Publishers
-  m_lookahead_pt_in_global_pub = this->create_publisher<visualization_msgs::msg::Marker>(
-      "control_pure_pursuit/lookahead_point", 1);
+  m_lookahead_pt_in_global_pub =
+      this->create_publisher<visualization_msgs::msg::Marker>(
+          "control_pure_pursuit/lookahead_point", 1);
 
   control_command = std::make_shared<nif::common::msgs::ControlCmd>();
 
@@ -17,9 +18,11 @@ ControlPurePursuitNode::ControlPurePursuitNode(const std::string &node_name)
     this->declare_parameter("invert_steer_direction", false);
     this->get_parameter("invert_steer_direction", m_param_is_steer_sign_inver);
 
-  } catch (const std::exception & e ) {
-//    TODO deciding whether or not to abort in this situation is critical
-    RCLCPP_ERROR(this->get_logger(), "Failed to declare or get parameters. Setting defaults values.");
+  } catch (const std::exception &e) {
+    //    TODO deciding whether or not to abort in this situation is critical
+    RCLCPP_ERROR(
+        this->get_logger(),
+        "Failed to declare or get parameters. Setting defaults values.");
     RCLCPP_ERROR(this->get_logger(), "What: %s", e.what());
 
     this->setNodeStatus(common::NodeStatusCode::NODE_FATAL_ERROR);
@@ -28,11 +31,8 @@ ControlPurePursuitNode::ControlPurePursuitNode(const std::string &node_name)
   }
 
   m_pure_pursuit_handler_ptr = std::make_shared<PurePursuit>(
-      m_param_min_lookahead_dist,
-      m_param_max_lookahead_dist,
-      m_param_lookahead_speed_ratio,
-      m_param_use_lpf_flg,
-      m_param_lfp_gain,
+      m_param_min_lookahead_dist, m_param_max_lookahead_dist,
+      m_param_lookahead_speed_ratio, m_param_use_lpf_flg, m_param_lfp_gain,
       m_param_is_steer_sign_inver);
 
   this->setNodeStatus(common::NodeStatusCode::NODE_INITIALIZED);
@@ -60,31 +60,30 @@ void ControlPurePursuitNode::publishLookAheadPointMarker() {
 }
 
 /**
- * solve() is called every time-step, and its result is published as output of the controller.
+ * solve() is called every time-step, and its result is published as output of
+ * the controller.
  *
  * @return reference to ControlCmd
  */
 nif::common::msgs::ControlCmd::SharedPtr ControlPurePursuitNode::solve() {
-//TODO update to comply with IControllerNode last version (embedded path subscriber and has...() methods)
-  if  (
-          (this->getReferenceTrajectory() && !(this->getReferenceTrajectory()->points.empty()))
-          ||
-          (this->hasReferencePath() && !(this->getReferencePath()->poses.empty()))
-      )
-  {
+  // TODO update to comply with IControllerNode last version (embedded path
+  // subscriber and has...() methods)
+  if ((this->getReferenceTrajectory() &&
+       !(this->getReferenceTrajectory()->trajectory_path.poses.empty())) ||
+      (this->hasReferencePath() &&
+       !(this->getReferencePath()->poses.empty()))) {
     auto pure_pursuit_steer_cmd_rad = m_pure_pursuit_handler_ptr->getSteerCmd();
     control_command->steering_control_cmd.data =
         pure_pursuit_steer_cmd_rad * nif::common::constants::RAD2DEG;
 
-    m_lookahead_pt_in_global = m_pure_pursuit_handler_ptr->getLookaheadPointInGlobal();
+    m_lookahead_pt_in_global =
+        m_pure_pursuit_handler_ptr->getLookaheadPointInGlobal();
     m_control_pt_in_body = m_pure_pursuit_handler_ptr->getControlPointInBody();
 
     this->publishLookAheadPointMarker();
 
     return control_command;
-  }
-  else
-  {
+  } else {
     RCLCPP_DEBUG(this->get_logger(), "%s\n",
                  "PURE PURSUIT : Ego status and Map track are not updated yet. "
                  "Send ZERO "
@@ -93,7 +92,6 @@ nif::common::msgs::ControlCmd::SharedPtr ControlPurePursuitNode::solve() {
     control_command->steering_control_cmd.data = 0.0;
     this->setNodeStatus(common::NODE_ERROR);
     return nullptr;
-
   }
 }
 void nif::control::ControlPurePursuitNode::afterReferencePathCallback() {
