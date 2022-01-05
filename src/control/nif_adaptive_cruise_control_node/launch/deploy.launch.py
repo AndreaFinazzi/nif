2e465525631e7d9b0a0ba37a39383677aec264a1
chanyoungjung <hy910915@kaist.ac.kr>
2022-01-05 08:27:31 -0800

    fix bugs in acc node

---
+++ src/control/nif_adaptive_cruise_control_node/launch/deploy.launch.py
@@ -0,0 +1,47 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+
+
+def get_share_file(package_name, file_name):
+    return os.path.join(get_package_share_directory(package_name), file_name)
+
+
+def generate_launch_description():
+
+    acc_rosparam_file = os.path.join(
+        get_package_share_directory('nif_adaptive_cruise_control_node'),
+        'config',
+        'rosparam.yaml'
+    )
+
+    nif_adaptive_cruise_control_param = DeclareLaunchArgument(
+        'nif_adaptive_cruise_control_param',
+        default_value=acc_rosparam_file,
+        description='Path to config file for nif_adaptive_cruise_control_param'
+    )
+
+    idm_acc_param_file = os.path.join(
+        get_package_share_directory('nif_adaptive_cruise_control_node'),
+        'config',
+        'idm_acc_config.yaml'
+    )
+
+    idm_based_control_node = Node(
+        package='nif_adaptive_cruise_control_node',
+        executable='nif_adaptive_cruise_control_node_exe',
+        output='screen',
+        parameters=[
+            LaunchConfiguration('nif_adaptive_cruise_control_param'),
+            {
+                'idm_acc_config_file': idm_acc_param_file
+            }
+            ]
+    )
+
+    return LaunchDescription([
+        nif_adaptive_cruise_control_param,
+        idm_based_control_node])
--- src/control/nif_adaptive_cruise_control_node/src/adaptive_cruise_node.cpp
+++ src/control/nif_adaptive_cruise_control_node/src/adaptive_cruise_node.cpp
@@ -53,10 +53,57 @@ IDMACCNode::IDMACCNode(const std::string &node_name_)
   this->m_idm_prt = std::make_shared<IDM>(m_config_file);
 }
 
-void IDMACCNode::egoTrajectoryCallback(
-    const nif_msgs::msg::DynamicTrajectory::SharedPtr traj_msg) {
+// void IDMACCNode::egoTrajectoryCallback(
+//     const nif_msgs::msg::DynamicTrajectory::SharedPtr traj_msg) {
 
-  m_ego_trajectory = *traj_msg;
+//   m_ego_trajectory = *traj_msg;
+
+//   m_ego_odom = this->getEgoOdometry();
+//   m_veh_speed_mps = m_ego_odom.twist.twist.linear.x;
+
+//   // prediction health check
+//   if (!m_oppo_pred_callback_first_run) {
+//     if (this->now() - m_prev_oppo_pred_last_update > rclcpp::Duration(2, 0)) {
+//       m_prediction_result.trajectory_path.poses.clear();
+//       m_prediction_result.trajectory_velocity.clear();
+//       m_prediction_result.trajectory_timestamp_array.clear();
+//       m_prediction_result.trajectory_global_progress.clear();
+//     }
+//   }
+
+//   if (m_prediction_result.trajectory_path.poses.empty() ||
+//       m_prediction_result.longi_planning_type !=
+//           m_prediction_result.LONGITUDINAL_PLANNING_TYPE_FOLLOW) {
+//     std_msgs::msg::Float32 out;
+//     out.data = nif::common::constants::numeric::INF;
+//     m_acc_cmd_publisher->publish(out);
+//   } else {
+//     std_msgs::msg::Float32 out;
+
+//     auto naive_gap = sqrt(
+//         pow(m_ego_odom.pose.pose.position.x -
+//                 m_prediction_result.trajectory_path.poses[0].pose.position.x,
+//             2) +
+//         pow(m_ego_odom.pose.pose.position.y -
+//                 m_prediction_result.trajectory_path.poses[0].pose.position.y,
+//             2));
+
+//     m_idm_prt->calcAccel(m_veh_speed_mps, naive_gap,
+//                          m_prediction_result.trajectory_velocity[0]);
+
+//     out.data = m_idm_prt->getACCCmd();
+//     m_acc_cmd_publisher->publish(out);
+//   }
+// }
+
+void IDMACCNode::predictionCallback(
+    const nif_msgs::msg::DynamicTrajectory::SharedPtr msg) {
+
+  if (m_oppo_pred_callback_first_run) {
+    m_oppo_pred_callback_first_run = false;
+  }
+  m_prev_oppo_pred_last_update = this->now(); // TODO msg->header.stamp;
+  m_prediction_result = *msg;
 
   m_ego_odom = this->getEgoOdometry();
   m_veh_speed_mps = m_ego_odom.twist.twist.linear.x;
@@ -88,6 +135,8 @@ void IDMACCNode::egoTrajectoryCallback(
                 m_prediction_result.trajectory_path.poses[0].pose.position.y,
             2));
 
+    std::cout << "naive_gap : " << naive_gap << std::endl;
+
     m_idm_prt->calcAccel(m_veh_speed_mps, naive_gap,
                          m_prediction_result.trajectory_velocity[0]);
 
@@ -96,30 +145,39 @@ void IDMACCNode::egoTrajectoryCallback(
   }
 }
 
-void IDMACCNode::predictionCallback(
-    const nif_msgs::msg::DynamicTrajectory::SharedPtr msg) {
+// void IDMACCNode::maptrackBodyCallback(
+//     const nav_msgs::msg::Path::SharedPtr msg) {
+//   m_maptrack_body = *msg;
+// }
 
-  if (m_oppo_pred_callback_first_run) {
-    m_oppo_pred_callback_first_run = false;
-  }
-  m_prev_oppo_pred_last_update = this->now(); // TODO msg->header.stamp;
-  m_prediction_result = *msg;
-}
+// void IDMACCNode::perceptionCallback(
+//     const nif_msgs::msg::Perception3DArray::SharedPtr msg) {
+//   m_perception_result = *msg;
 
-void IDMACCNode::maptrackBodyCallback(
-    const nav_msgs::msg::Path::SharedPtr msg) {
-  m_maptrack_body = *msg;
-}
-
-// void IDMACCNode::detectionCallback(
-//     const nif_msgs::msg::DetectedObjectArray::SharedPtr det_msg) {
-//   m_det_result = *det_msg;
+//   // TODO : ASSUMING THAT ONLY CIPV INFORMATION IS IN THE MESSAGE
 //   int cipv_idx = 0;
 
+//   // std::cout << "body cipv x : " <<
+//   // m_perception_result.perception_list[cipv_idx].detection_result_3d.center.position.x
+//   // << std::endl;
+
+//   if (sqrt(pow(m_perception_result.perception_list[cipv_idx]
+//                    .detection_result_3d.center.position.x,
+//                2) +
+//            pow(m_perception_result.perception_list[cipv_idx]
+//                    .detection_result_3d.center.position.y,
+//                2)) > 100.0) {
+//     // opponent is behind us. dont care about the ACC
+//     m_acc_cmd = m_idm_prt->getParamAccelMax();
+//     std_msgs::msg::Float32 out;
+//     out.data = m_acc_cmd;
+//     m_acc_cmd_publisher->publish(out);
+//     return;
+//   }
+
 //   // Calc acc cmd
-//   // m_veh_speed_mps = this->getEgoPowertrainState().vehicle_speed_kmph
-//   / 3.6; m_ego_odom = this->getEgoOdometry(); m_veh_speed_mps =
-//   m_ego_odom.twist.twist.linear.x;
+//   m_veh_speed_mps = this->getEgoOdometry().twist.twist.linear.x;
+//   m_ego_odom = this->getEgoOdometry();
 
 //   // TODO : Assigning CIPV
 //   // NOTE : In CES, there is only one opponent on the track.
@@ -133,30 +191,29 @@ void IDMACCNode::maptrackBodyCallback(
 //   // no, converte to the body frame)
 
 //   if (this->hasEgoOdometry() && this->hasEgoPowertrainState()) {
-//     if (det_msg->objects.size() != 0) {
+//     if (msg->perception_list.size() != 0) {
 //       // NOTE : This is just for the test. Proecssing of CIPC data should be
 //       // done.
 
-//       // TODO : The way to calculate the progress gap btw the ego and
-//       opponent. double progress_gap = 0.0;
+//       // TODO : The way to calculate the progress gap btw the ego and opponent.
+//       double progress_gap = 0.0;
 
 //       // Approach 1. (longitudinal-wise distance directly from the perception
 //       // result)
-//       progress_gap = det_msg->objects[cipv_idx].pose.position.x;
+//       progress_gap =
+//           msg->perception_list[cipv_idx].detection_result_3d.center.position.x;
 //       if (progress_gap < 0.0) {
-//         // when the car is behind us, don't care about the ACC. Set the
-//         progress
+//         // when the car is behind us, don't care about the ACC. Set the progress
 //         // gap as INF
 //         progress_gap = nif::common::constants::numeric::INF;
 //       }
 
 //       // Approach 2. (Based on our future trajectory, calculate the progress.
-//       // But when the case that we want to overtake, the progress gap might
-//       be
+//       // But when the case that we want to overtake, the progress gap might be
 //       // wrong.)
-//       m_idm_prt->calcAccel(m_veh_speed_mps,
-//                            progress_gap,
-//                            det_msg->objects[cipv_idx].velocity.linear.x);
+//       m_idm_prt->calcAccel(
+//           m_veh_speed_mps, progress_gap,
+//           msg->perception_list[cipv_idx].obj_velocity_in_local.linear.x);
 
 //       m_acc_cmd = m_idm_prt->getACCCmd();
 //     } else {
@@ -171,82 +228,3 @@ void IDMACCNode::maptrackBodyCallback(
 //   out.data = m_acc_cmd;
 //   m_acc_cmd_publisher->publish(out);
 // }
-
-void IDMACCNode::perceptionCallback(
-    const nif_msgs::msg::Perception3DArray::SharedPtr msg) {
-  m_perception_result = *msg;
-
-  // TODO : ASSUMING THAT ONLY CIPV INFORMATION IS IN THE MESSAGE
-  int cipv_idx = 0;
-
-  // std::cout << "body cipv x : " <<
-  // m_perception_result.perception_list[cipv_idx].detection_result_3d.center.position.x
-  // << std::endl;
-
-  if (sqrt(pow(m_perception_result.perception_list[cipv_idx]
-                   .detection_result_3d.center.position.x,
-               2) +
-           pow(m_perception_result.perception_list[cipv_idx]
-                   .detection_result_3d.center.position.y,
-               2)) > 100.0) {
-    // opponent is behind us. dont care about the ACC
-    m_acc_cmd = m_idm_prt->getParamAccelMax();
-    std_msgs::msg::Float32 out;
-    out.data = m_acc_cmd;
-    m_acc_cmd_publisher->publish(out);
-    return;
-  }
-
-  // Calc acc cmd
-  m_veh_speed_mps = this->getEgoOdometry().twist.twist.linear.x;
-  m_ego_odom = this->getEgoOdometry();
-
-  // TODO : Assigning CIPV
-  // NOTE : In CES, there is only one opponent on the track.
-  // At the moment, we assumed that there is no false positive.
-
-  // First approach
-  // 1. Convert opponent's position to the global
-  // 2. Project to the ego's racing line to calculate the progress
-  // 3. Calculate the progress gap
-  // * double check that the detection message is represent in body frame (if
-  // no, converte to the body frame)
-
-  if (this->hasEgoOdometry() && this->hasEgoPowertrainState()) {
-    if (msg->perception_list.size() != 0) {
-      // NOTE : This is just for the test. Proecssing of CIPC data should be
-      // done.
-
-      // TODO : The way to calculate the progress gap btw the ego and opponent.
-      double progress_gap = 0.0;
-
-      // Approach 1. (longitudinal-wise distance directly from the perception
-      // result)
-      progress_gap =
-          msg->perception_list[cipv_idx].detection_result_3d.center.position.x;
-      if (progress_gap < 0.0) {
-        // when the car is behind us, don't care about the ACC. Set the progress
-        // gap as INF
-        progress_gap = nif::common::constants::numeric::INF;
-      }
-
-      // Approach 2. (Based on our future trajectory, calculate the progress.
-      // But when the case that we want to overtake, the progress gap might be
-      // wrong.)
-      m_idm_prt->calcAccel(
-          m_veh_speed_mps, progress_gap,
-          msg->perception_list[cipv_idx].obj_velocity_in_local.linear.x);
-
-      m_acc_cmd = m_idm_prt->getACCCmd();
-    } else {
-      m_acc_cmd = m_idm_prt->getParamAccelMax();
-    }
-  } else {
-    // abnormal situation
-    m_acc_cmd = 0.0;
-  }
-
-  std_msgs::msg::Float32 out;
-  out.data = m_acc_cmd;
-  m_acc_cmd_publisher->publish(out);
-}
